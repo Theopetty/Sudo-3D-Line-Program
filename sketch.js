85c1dd9634a31fed6597b42d0201dd3b2ccb3d44
Theopetty <55097456+Theopetty@users.noreply.github.com>
2019-10-15 09:26:34 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,6 +3,7 @@ let y=0;
 let x2=0
 let y2=0;
 let mode=1;
+let lk=0;
 let p6o7=0;
 function setup(){
 createCanvas(700,700);
@@ -17,7 +18,7 @@ if(key=='w'){if (mode==1){y=y-20}if (mode==3){y2=y2-20}
   if(key=='s'){if (mode==1){y=y+20}if (mode==3){y2=y2+20}}
   if(key=='d'){if (mode==1){x=x+10}if (mode==3){x2=x2+10}}
 if (mode==4){}else{if (mode==7){}else{
-
+if (key==0){}else{lk=key;}
   
   line(x,y,mouseX,mouseY);}}
   if (mode==4){}else{if (mode==6){}else{
@@ -27,6 +28,7 @@ if (key=='a'){}else{
 if (key=='s'){}else{
 if (key=='d'){}else{
 if (key==0){}else{
-mode=key;if (key=='4'){p6o7=2;}if (key=='6'){if (p6o7==2){}else{p6o7=0;background(0,250,0)}}if (key=='7'){if (p6o7==2){}else{p6o7=0;background(0,250,0)}}
+mode=key;if (key=='4'){c=1;}if (key=='6'){if (c==0){background(0,250,0);c=1;}}
+if (key=='7'){if (c==0){background(0,250,0);c=1;}}
 }}}}}}
-function keyReleased(){key=0;}
+function keyReleased(){key=0;if (mode==4){}else{c=0}}
